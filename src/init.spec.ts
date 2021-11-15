3498b990c07e04a4ecc3c941b39822c2eb50b380
Lee Goddard <leegee@gmail.com>
2021-11-15 13:34:01 +0100

    change perspective

--- src/init.spec.ts
+++ src/init.spec.ts
@@ -3,8 +3,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 beforeAll(async () => {
-  const assassinate = await prisma.verb.create({
-    data: { name: 'assassinate' }
+  const assassinates = await prisma.verb.create({
+    data: { name: 'assassinates' }
+  });
+
+  const hosts = await prisma.verb.create({
+    data: { name: 'hosts' }
   });
 
   const jfk = await prisma.person.create({
@@ -15,11 +19,23 @@ beforeAll(async () => {
     data: { fullname: 'Lee Harvey Oswald', },
   });
 
-  const relation = await prisma.relations.create({
+  const arthur = await prisma.person.create({
+    data: { fullname: 'Arthur Young', },
+  });
+
+  await prisma.relations.create({
     data: {
-      verbId: assassinate.id,
-      objectId: jfk.id,
-      subjectId: oswald.id,
+      verbId: assassinates.id,
+      subjectId: jfk.id,
+      objectId: oswald.id,
+    },
+  });
+
+  await prisma.relations.create({
+    data: {
+      verbId: hosts.id,
+      subjectId: arthur.id,
+      objectId: oswald.id,
     },
   });
 });
@@ -28,52 +44,37 @@ afterAll(async () => {
   await prisma.relations.deleteMany();
   await prisma.verb.deleteMany();
   await prisma.person.deleteMany();
-  await prisma.$disconnect()
+  await prisma.$disconnect();
 });
 
 
 describe('Initial scheme', () => {
   it('has people', async () => {
     const allUsers = await prisma.person.findMany();
-    expect(allUsers).toEqual([
-      {
-        "fullname": "John F Kennedy",
-        "id": 1,
-        "published": false,
-      },
-      {
-        "fullname": "Lee Harvey Oswald",
-        "id": 2,
-        "published": false,
-      },
-    ])
+    expect(allUsers).toHaveLength(3);
+    expect(allUsers[0].fullname).toBe("John F Kennedy");
+    expect(allUsers[1].fullname).toBe("Lee Harvey Oswald");
+    expect(allUsers[2].fullname).toBe("Arthur Young");
   });
 
   it('has people with relations', async () => {
-    const allUsers = await prisma.person.findMany({
-      include: {
-        Friends: {
-          include: {
-            Verb: true
-          }
-        }
+    const rv = await prisma.relations.findMany({
+      select: {
+        Subject: true,
+        Object: true,
+        Verb: true
       }
     });
 
-    console.dir(allUsers, { depth: null });
+    expect(rv).toHaveLength(2);
 
-    expect(allUsers).toEqual([
-      {
-        "fullname": "John F Kennedy",
-        "id": 1,
-        "published": false,
-      },
-      {
-        "fullname": "Lee Harvey Oswald",
-        "id": 2,
-        "published": false,
-      },
-    ])
+    expect(rv[0].Subject.fullname).toBe('John F Kennedy');
+    expect(rv[0].Object.fullname).toBe('Lee Harvey Oswald');
+    expect(rv[0].Verb.name).toBe('assassinates');
+
+    expect(rv[1].Subject.fullname).toBe('Arthur Young');
+    expect(rv[1].Object.fullname).toBe('Lee Harvey Oswald');
+    expect(rv[1].Verb.name).toBe('hosts');
   });
 });
 
