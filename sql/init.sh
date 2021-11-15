3e7b486b40e499df742fc8a9a84c7bf864978e6b
Lee Goddard <leegee@gmail.com>
2021-11-15 08:24:39 +0100

    model with prisma, not sql

--- sql/init.sh
+++ sql/init.sh
@@ -3,11 +3,12 @@ source "./.env"
 echo DB_USER $DB_USER
 echo DB_NAME $DB_NAME
 
-# docker exec -it postgres psql -U $DB_USER -c "CREATE DATABASE $DB_NAME ENCODING 'UTF8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8'"
+# docker exec -it postgres psql -U $DB_USER -c "DROP DATABASE $DB_NAME"
+docker exec -it postgres psql -U $DB_USER -c "CREATE DATABASE $DB_NAME ENCODING 'UTF8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8'"
 
 docker cp ./sql/scheme.sql postgres:scheme.sql
 
-docker exec -it postgres psql -U $DB_USER -a $DB_NAME -f scheme.sql
+# docker exec -it postgres psql -U $DB_USER -a $DB_NAME -f scheme.sql
 
 # for sql_file in ./sql/deltas/*.sql
 # do
