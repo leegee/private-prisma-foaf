a3eaa3c6a10620c7f45820b45959cddc886e90bc
Lee Goddard <leegee@gmail.com>
2021-11-15 11:19:05 +0100

    test

--- src/init.spec.ts
+++ src/init.spec.ts
@@ -2,24 +2,45 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+beforeAll(async () => {
+  const assassin = await prisma.verb.create({
+    data: { name: 'assassin' }
+  });
+
+  const jfk = await prisma.person.create({
+    data: { fullname: 'John F Kennedy', },
+  });
+
+  const oswald = await prisma.person.create({
+    data: { fullname: 'Lee Harvey Oswald', },
+  });
+
+
+});
+
+afterAll(async () => {
+  await prisma.person.deleteMany();
+  await prisma.verb.deleteMany();
+  await prisma.$disconnect()
+});
+
+
 describe('Initial scheme', () => {
   it('has people', async () => {
-    const allUsers = await prisma.person.findMany()
-    expect(allUsers).toEqual([])
+    const allUsers = await prisma.person.findMany();
+    expect(allUsers).toEqual([
+      {
+        "fullname": "John F Kennedy",
+        "id": 1,
+        "published": false,
+      },
+      {
+        "fullname": "Lee Harvey Oswald",
+        "id": 2,
+        "published": false,
+      },
+    ])
   });
 });
 
 
-
-// async function main() {
-//   const allUsers = await prisma.person.findMany()
-//   console.dir(allUsers, { depth: null })
-// }
-
-// main()
-//   .catch(e => {
-//     throw e
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect()
-//   });
