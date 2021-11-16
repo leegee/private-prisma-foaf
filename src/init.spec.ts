60da98a4f14703d144e283fae7eba28f6628fca7
Lee Goddard <leegee@gmail.com>
2021-11-16 10:41:13 +0100

    simplify action dates

--- src/init.spec.ts
+++ src/init.spec.ts
@@ -12,36 +12,42 @@ beforeAll(async () => {
   });
 
   const jfk = await prisma.person.create({
-    data: { fullname: 'John F Kennedy', },
+    data: { knownas: 'John F Kennedy', },
   });
 
   const oswald = await prisma.person.create({
-    data: { fullname: 'Lee Harvey Oswald', },
+    data: { knownas: 'Lee Harvey Oswald', },
   });
 
   const arthur = await prisma.person.create({
-    data: { fullname: 'Arthur Young', },
+    data: { knownas: 'Arthur Young', },
   });
 
-  await prisma.relations.create({
+  await prisma.action.create({
     data: {
       verbId: assassinates.id,
       subjectId: jfk.id,
       objectId: oswald.id,
+      start: new Date('1963-11-22'),
+      end: new Date('1963-11-22'),
     },
   });
 
-  await prisma.relations.create({
+  // https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#create-a-new-profile-record-then-connect-it-to-an-existing-user-record-or-create-a-new-user
+  await prisma.action.create({
     data: {
       verbId: hosts.id,
       subjectId: arthur.id,
       objectId: oswald.id,
+      start: new Date('1963-11-21'),
+      end: new Date('1963-11-22'),
     },
   });
+
 });
 
 afterAll(async () => {
-  await prisma.relations.deleteMany();
+  await prisma.action.deleteMany();
   await prisma.verb.deleteMany();
   await prisma.person.deleteMany();
   await prisma.$disconnect();
@@ -52,13 +58,13 @@ describe('Initial scheme', () => {
   it('has people', async () => {
     const allUsers = await prisma.person.findMany();
     expect(allUsers).toHaveLength(3);
-    expect(allUsers[0].fullname).toBe("John F Kennedy");
-    expect(allUsers[1].fullname).toBe("Lee Harvey Oswald");
-    expect(allUsers[2].fullname).toBe("Arthur Young");
+    expect(allUsers[0].knownas).toBe("John F Kennedy");
+    expect(allUsers[1].knownas).toBe("Lee Harvey Oswald");
+    expect(allUsers[2].knownas).toBe("Arthur Young");
   });
 
-  it('has people with relations', async () => {
-    const rv = await prisma.relations.findMany({
+  it('has people with action', async () => {
+    const rv = await prisma.action.findMany({
       select: {
         Subject: true,
         Object: true,
@@ -68,12 +74,12 @@ describe('Initial scheme', () => {
 
     expect(rv).toHaveLength(2);
 
-    expect(rv[0].Subject.fullname).toBe('John F Kennedy');
-    expect(rv[0].Object.fullname).toBe('Lee Harvey Oswald');
+    expect(rv[0].Subject.knownas).toBe('John F Kennedy');
+    expect(rv[0].Object.knownas).toBe('Lee Harvey Oswald');
     expect(rv[0].Verb.name).toBe('assassinates');
 
-    expect(rv[1].Subject.fullname).toBe('Arthur Young');
-    expect(rv[1].Object.fullname).toBe('Lee Harvey Oswald');
+    expect(rv[1].Subject.knownas).toBe('Arthur Young');
+    expect(rv[1].Object.knownas).toBe('Lee Harvey Oswald');
     expect(rv[1].Verb.name).toBe('hosts');
   });
 });
