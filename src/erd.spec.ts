65a73b1b9e71b9c92e29281a7e1683f6256455b7
Lee Goddard <leegee@gmail.com>
2021-11-19 10:13:33 +0100

    custom error

--- src/erd.spec.ts
+++ src/erd.spec.ts
@@ -5,7 +5,7 @@
 import { Person, Action } from '@prisma/client';
 import { MockProxy } from 'jest-mock-extended';
 import { mockPrisma } from 'testlib/mock-prisma';
-import { Erd } from './erd';
+import { Erd, PersonNotFoundError } from './erd';
 
 const actionFixture: MockProxy<Action> = {
   start: new Date(),
@@ -27,17 +27,27 @@ const personFixture: MockProxy<Person> = {
 };
 
 describe('erd', () => {
-  it('_getActions', async () => {
+  describe('_getActions', () => {
+    it('throws the correct error when person not found', async () => {
+      mockPrisma.person.findFirst.mockResolvedValue(null);
 
-    mockPrisma.action.findMany.mockResolvedValue([actionFixture]);
-    mockPrisma.person.findFirst.mockResolvedValue(personFixture);
+      const erd = new Erd({ prisma: mockPrisma, knownas: 'no person' });
 
-    const erd = new Erd({ prisma: mockPrisma, knownas: personFixture.knownas, });
-    const actionsArray = await erd._getActions();
+      await expect(
+        erd._getActions()
+      ).rejects.toBeInstanceOf(PersonNotFoundError);
+    });
 
-    expect(actionsArray).toBeDefined();
-    expect(actionsArray).toHaveLength(1);
-    expect(actionsArray[0]).toEqual(actionFixture);
+    it('returns actions', async () => {
+      mockPrisma.action.findMany.mockResolvedValue([actionFixture]);
+      mockPrisma.person.findFirst.mockResolvedValue(personFixture);
+
+      const erd = new Erd({ prisma: mockPrisma, knownas: personFixture.knownas, });
+      const actionsArray = await erd._getActions();
+
+      expect(actionsArray).toBeDefined();
+      expect(actionsArray).toHaveLength(1);
+      expect(actionsArray[0]).toEqual(actionFixture);
+    });
   });
-
 });
--- src/erd.ts
+++ src/erd.ts
@@ -6,36 +6,40 @@ import fs from 'fs';
 import os from 'os';
 import { PrismaClient, Prisma, Person } from '@prisma/client';
 
+export class PersonNotFoundError extends Error {
+  constructor(message: string) {
+    super(`No such person knownas "${message}".`);
+    Object.setPrototypeOf(this, PersonNotFoundError.prototype);
+  }
+}
+
 export interface IErdArgs {
   prisma: PrismaClient<
     Prisma.PrismaClientOptions,
     never,
     Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
-  >,
-  knownas: string,
-  savepath?: string,
+  >;
+  knownas: string;
+  savepath?: string;
 }
-
 export class Erd {
-  prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>;
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
   knownas: string;
   savepath?: string;
   personId: number | undefined;
   tmpDir = fs.mkdtempSync(os.tmpdir() + path.sep + 'person-erd-');
   theme = 'forest';
 
-  constructor(
-    {
-      prisma,
-      knownas,
-      savepath,
-    }: IErdArgs
-  ) {
+  constructor({ prisma, knownas, savepath }: IErdArgs) {
     this.prisma = prisma;
     this.knownas = knownas;
     if (savepath) {
-      this.savepath = savepath
-    };
+      this.savepath = savepath;
+    }
   }
 
   async createString(): Promise<string> {
@@ -86,39 +90,23 @@ export class Erd {
         (action as any).Object.knownas +
         ']' +
         '\n';
-      // } else {
-      //   mermaid +=
-      //     'Person' +
-      //     (action as any).Subject.id +
-      //     '[' +
-      //     (action as any).Subject.knownas +
-      //     ']-->|' +
-      //     (action as any).Verb.name +
-      //     '|Person' +
-      //     (action as any).Object.id +
-      //     '[' +
-      //     (action as any).Object.knownas +
-      //     ']' +
-      //     '\n';
-      // }
     });
 
     return mermaid;
   }
 
-
   async _getActions() {
-    if (!!this.personId) {
+    if (!this.personId) {
       const person = await this.prisma.person.findFirst({
         where: { knownas: this.knownas },
         select: { id: true },
       });
 
-      if (!person) {
-        throw new Error(`No such person knownas "${this.knownas}"`);
+      if (person === null) {
+        throw new PersonNotFoundError(this.knownas);
       }
 
-      this.personId = person.id;
+      this.personId = person?.id;
     }
 
     const actions = await this.prisma.action.findMany({
@@ -134,7 +122,7 @@ export class Erd {
   }
 
   _composeGraph(graphedActions: string): string {
-    return 'graph TD\n' + graphedActions + "\n";
+    return 'graph TD\n' + graphedActions + '\n';
   }
 
   _save(graph: string): void {
@@ -142,11 +130,16 @@ export class Erd {
       throw new Error('savepath was not supplied during construction');
     }
 
-    const tempMermaidFile = path.resolve(path.join(this.tmpDir, 'person-erd.mmd'));
+    const tempMermaidFile = path.resolve(
+      path.join(this.tmpDir, 'person-erd.mmd'),
+    );
     fs.writeFileSync(tempMermaidFile, graph);
 
     const tempConfigFile = path.resolve(path.join(this.tmpDir, 'config.json'));
-    fs.writeFileSync(tempConfigFile, JSON.stringify({ deterministicIds: true }));
+    fs.writeFileSync(
+      tempConfigFile,
+      JSON.stringify({ deterministicIds: true }),
+    );
 
     const mermaidCliNodePath = path.resolve(
       path.join('node_modules', '.bin', 'mmdc'),
