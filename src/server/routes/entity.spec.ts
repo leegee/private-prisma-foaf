aeb9fba9e353e16305a46bea82b2f3cb8aec35dc
Lee Goddard <leegee@gmail.com>
2021-12-03 15:24:13 +0100

    Pactum test configs

--- src/server/routes/entity.spec.ts
+++ src/server/routes/entity.spec.ts
@@ -1,32 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { pactum } from 'testlib/pactum';
 
-import { start as startServer } from 'src/server';
-import PrismaTestEnvironment from 'testlib/prisma-test-env';
-
-import Console from 'console';
-console.log = Console.log;
-console.info = Console.info;
-console.debug = Console.debug;
-console.warn = Console.warn;
-
-PrismaTestEnvironment.init();
-
-let server: FastifyInstance;
-
-jest.setTimeout(1000 * 10);
-
-beforeAll(async () => {
-  // await pactum.mock.start(4000);
-  server = await startServer();
-  pactum.request.setBaseUrl(`http://localhost:${process.env.ERD_PORT || 3000}`);
-});
-
-afterAll(async () => {
-  // await pactum.mock.stop();
-  server.close();
-});
-
 
 describe('GET /entity?q=', () => {
 
--- test/lib/pactum.ts
+++ test/lib/pactum.ts
@@ -1,11 +1,38 @@
 import pactum from 'pactum';
+import { FastifyInstance } from 'fastify';
 
-type Ctx = {
+import PrismaTestEnvironment from "testlib/prisma-test-env";
+import { start as startServer } from 'src/server';
+
+type assertionHandlerCtx = {
   [key: string]: any;
 };
 
-pactum.handler.addAssertHandler('type', (ctx: Ctx) => {
+PrismaTestEnvironment.init();
+jest.setTimeout(1000 * 30);
+
+pactum.handler.addAssertHandler('type', (ctx: assertionHandlerCtx) => {
   return typeof ctx.data === ctx.args[0];
 });
 
 export { pactum };
+
+import Console from 'console';
+console.log = Console.log;
+console.info = Console.info;
+console.debug = Console.debug;
+console.warn = Console.warn;
+
+
+let server: FastifyInstance;
+
+beforeAll(async () => {
+  // await pactum.mock.start(4000);
+  server = await startServer();
+  pactum.request.setBaseUrl(`http://localhost:${process.env.ERD_PORT || 3000}`);
+});
+
+afterAll(async () => {
+  // await pactum.mock.stop();
+  server.close();
+});
