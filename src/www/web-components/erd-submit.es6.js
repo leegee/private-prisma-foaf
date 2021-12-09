32e3ae3784aab5e191319b35aee9c1a3c8d2c2a9
Lee Goddard <leegee@gmail.com>
2021-12-09 14:26:38 +0100

    tests caught typo

--- src/www/web-components/erd-submit.es6.js
+++ src/www/web-components/erd-submit.es6.js
@@ -5,15 +5,24 @@ import { ErdBaseElement } from './erd-base-element.es6.js';
 class ErdSubmit extends ErdBaseElement {
   static elName = 'erd-submit';
   static suggestionsJsonKey = 'entities';
+  static get observedAttributes() { return ['disabled']; }
+
+  set disabled(val) {
+    if (this.el) {
+      this.el.setAttribute('disabled', val);
+    }
+  }
 
   el = undefined;
 
   async connectedCallback() {
     await super.connectedCallback();
     this.apiurl = this.getAttribute('apiurl').replace(/\/+$/, '/predicate');
-    this.el = this.shadow.querySelector('button').addEventListener(
+    this.el = this.shadow.querySelector('button');
+    this.el.addEventListener(
       'click', this.submit.bind(this)
     );
+    this.disabled = true;
   }
 
   submit() {
